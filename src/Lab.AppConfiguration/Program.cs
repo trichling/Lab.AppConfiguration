51ca5d2feb2b53d20e895eb20d08ad601a1b4e3b
Tobias Richling <RichlinT@apetito.com>
2020-08-01 21:14:44 +0200

    remove keys from configs, switch to principal

--- src/Lab.AppConfiguration/Program.cs
+++ src/Lab.AppConfiguration/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace Lab.AppConfiguration
 {
@@ -26,15 +28,30 @@ namespace Lab.AppConfiguration
                         // hostingContext.HostingEnvironment.IsProduction()
 
                         var settings = config.Build();
+                        //var azureAppConfigConnectionString = settings["ConnectionStrings:AppConfig"];
+
+                        var clientId = settings["Azure:LeckeritoServicePrinzipal:ClientId"];
+                        var clientSecret = settings["Azure:LeckeritoServicePrinzipal:ClientSecret"];
+                        var tenantId = settings["Azure:LeckeritoServicePrinzipal:TenantId"];
+
+                        var credentials = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
                         config.AddAzureAppConfiguration(options => {
+                            options.Connect(new Uri("https://leckeritoappsettings.azconfig.io/"), credentials)
                             // Connect with ConnectionString from User Secrets
-                            options.Connect(settings["ConnectionStrings:AppConfig"])
+                            //options.Connect(azureAppConfigConnectionString)
                                     // For live updates
                                     .ConfigureRefresh(refresh => 
                                     {
                                         refresh.Register("leckerito:Lab:AppSettings:ReloadSentinel", refreshAll: true)
                                                 .SetCacheExpiration(new TimeSpan(0, 0, 3));
-                                    });
+                                    })
+                                    .ConfigureKeyVault(kv => {
+                                        kv.SetCredential(credentials);
+                                    })
+                                    .Select("leckerito:Lab:AppSettings:*", LabelFilter.Null)
+                                    // Override with any configuration values specific to current hosting env
+                                    .Select("leckerito:Lab:AppSettings:*", hostingContext.HostingEnvironment.EnvironmentName);
                         });
                     });
 
